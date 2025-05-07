e298ce96ed912d9dcb9ac8ea017d9f9a3dbb4ecc
xcindyy <98561162+xcindyy@users.noreply.github.com>
2025-05-06 22:15:27 -0600

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 21 14:28:08 2021
+
+@author: owner
+"""
+
+print("This is Cindy Yin in 2021")
+print("Let us finish a course!")
